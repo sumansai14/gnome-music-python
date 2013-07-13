6fcefc6175a11c1b6485e7975d5d19cb53681126
Sai <suman.sai14@gmail.com>
2013-07-13 13:13:52 +0530

    fixed syntax errors in application windows and player.

--- gnome-music/application.py
+++ gnome-music/application.py
@@ -4,24 +4,25 @@ from gi.repository import Gtk, Gio, GLib
 
 from window import Window
 
+
 class Application(Gtk.Application):
     def __init__(self):
         Gtk.Application.__init__(self,
-            application_id='org.gnome.Music',
-            flags=Gio.ApplicationFlags.FLAGS_NONE,
-            inactivity_timeout=12000)
+                                 application_id='org.gnome.Music',
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE,
+                                 inactivity_timeout=12000)
         GLib.set_application_name("Music")
 
     def do_startup(self):
         Gtk.Application.startup(self)
 
     def do_activate(self):
-        if(!self._window):
+        if self._window is not None:
             self._window = Window(self)
         self._window.present()
 
     def Quit(self):
-        this.quit();
+        self.quit()
 
 if __name__ == '__main__':
     app = Application()
